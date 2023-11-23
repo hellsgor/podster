f8e58aed7a3ca1982b8576d72c3ce529c41ec639
istorchevoi <istorchevoi@axbit.ru>
2023-11-23 11:58:40 +0400

    feat: [PODSTER-100] - функция показа модального окна

--- src/constants/names-and-ids.js
+++ src/constants/names-and-ids.js
@@ -1,12 +1,18 @@
-export const REGISTRATION_CONTROLS = {
-  ADVERTISER_TYPE: 'user-reg-type',
-  ADVERTISER_INN: 'user-inn',
-  ADVERTISER_NAME: 'user-name',
-  ADVERTISER_OGRN: 'user-ogrn',
-  ADVERTISER_OKVED: 'user-okved',
-  ADVERTISER_EMAIL: 'user-email',
-  ADVERTISER_PASSWORD: 'user-password',
-  ADVERTISER_PASSWORD_REPEATED: 'user-password-repeated',
-  ADVERTISER_AGREEMENT_DATA_PROCESSING: 'agreement-data-processing',
-  ADVERTISER_AGREEMENT_CONTRACT: 'agreement-contract',
+export const REGISTRATION_IDS = {
+  REGISTRATION_CONTROLS: {
+    ADVERTISER_TYPE: 'user-reg-type',
+    ADVERTISER_INN: 'user-inn',
+    ADVERTISER_NAME: 'user-name',
+    ADVERTISER_OGRN: 'user-ogrn',
+    ADVERTISER_OKVED: 'user-okved',
+    ADVERTISER_EMAIL: 'user-email',
+    ADVERTISER_PASSWORD: 'user-password',
+    ADVERTISER_PASSWORD_REPEATED: 'user-password-repeated',
+    ADVERTISER_AGREEMENT_DATA_PROCESSING: 'agreement-data-processing',
+    ADVERTISER_AGREEMENT_CONTRACT: 'agreement-contract',
+    ADVERTISER_AGREEMENT_CONTRACT_BUTTON: 'agreement-contract-button',
+  },
+  REGISTRATION_MODALS: {
+    CONTRACT_MODAL: 'contract-modal',
+  },
 };
--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -2,3 +2,14 @@ import 'UIKit/index.js';
 import 'Constants/index.js';
 import 'Components/registration/buttons-modal/buttons-modal.js';
 import 'Components/common/modal/modal.js';
+import {showModal} from 'Components/common/modal/_show-modal';
+import {REGISTRATION_IDS} from 'Constants/names-and-ids';
+
+const agreementContractButton = document.getElementById(
+  REGISTRATION_IDS.REGISTRATION_CONTROLS.ADVERTISER_AGREEMENT_CONTRACT_BUTTON
+);
+
+agreementContractButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  showModal(REGISTRATION_IDS.REGISTRATION_MODALS.CONTRACT_MODAL);
+});
