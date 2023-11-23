71230e5f64dc24013aa4952a11919d333959f9c6
istorchevoi <istorchevoi@axbit.ru>
2023-11-23 14:39:47 +0400

    feat: [PODSTER-100] - функция принятия докумнета

--- src/components/registration/buttons-modal/_accept-document.js
+++ src/components/registration/buttons-modal/_accept-document.js
@@ -2,7 +2,6 @@ import {closeModal} from 'Components/common/modal/_close-modal';
 
 export function acceptDocument(event) {
   const modal = event.target.closest('.modal');
-
   document.querySelector(`input[data-modal-id="${modal.id}"]`).checked = true;
   closeModal(modal.id);
 }
