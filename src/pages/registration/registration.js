5d1f626805186228a9c0cd25ccd7e7d239049867
istorchevoi <istorchevoi@axbit.ru>
2023-11-27 10:39:36 +0400

    feat: [PODSTER-100] - Маска "только цифры"

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -6,6 +6,7 @@ import {REGISTRATION_IDS} from 'Constants/names-and-ids';
 import {addListenerForModal} from 'Components/registration/helpers/_addListenerForModal';
 import {momentValidation} from 'Components/registration/helpers/validation/_momentValidation';
 import {getValidatedControls} from 'Components/registration/helpers/_getValidatedControls';
+import {numbersOnly} from 'Components/registration/helpers/masks/numbers-only';
 
 addListenerForModal(
   REGISTRATION_IDS.REGISTRATION_CONTROLS.ADVERTISER_AGREEMENT_CONTRACT_BUTTON,
@@ -26,6 +27,15 @@ getValidatedControls().forEach((control) => {
         : 'change'
       : 'change',
     (event) => {
+      if (
+        event.target.name ===
+          REGISTRATION_IDS.REGISTRATION_CONTROLS.ADVERTISER_INN ||
+        event.target.name ===
+          REGISTRATION_IDS.REGISTRATION_CONTROLS.ADVERTISER_OGRN ||
+        event.target.name ===
+          REGISTRATION_IDS.REGISTRATION_CONTROLS.ADVERTISER_OKVED
+      )
+        numbersOnly(event.target);
       momentValidation(event, REGISTRATION_IDS.REGISTRATION_CONTROLS);
     }
   );
