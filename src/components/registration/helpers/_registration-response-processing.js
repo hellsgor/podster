c34023e1b7b6d8d127ceba9efe2f30ac5f66be86
istorchevoi <istorchevoi@axbit.ru>
2023-12-01 00:37:07 +0400

    feat: [PODSTER-100] - Переход на страницу "Спасибо за регистрацию" в случае успешного ответа

--- src/components/registration/helpers/_registration-response-processing.js
+++ src/components/registration/helpers/_registration-response-processing.js
@@ -1,4 +1,10 @@
+import {isSuccess} from 'Utils/handle-form-submit/_isSuccess';
+
 export function registrationResponseProcessing(response) {
   console.log('reg ok');
   console.log(response);
+
+  if (isSuccess(response)) {
+    window.location.href = './registrationThanks.html';
+  }
 }
