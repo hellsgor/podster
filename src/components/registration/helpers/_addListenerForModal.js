10724da0e194c3e8ca75339a9234c1112efc6ef8
istorchevoi <istorchevoi@axbit.ru>
2023-11-30 11:17:11 +0400

    feat: [PODSTER-100] - Запрет прокручивания <body> при открытых модальных окнах

--- src/components/registration/helpers/_addListenerForModal.js
+++ src/components/registration/helpers/_addListenerForModal.js
@@ -3,6 +3,7 @@ import {showModal} from 'Components/common/modal/_show-modal';
 export function addListenerForModal(buttonID, modalID) {
   document.getElementById(buttonID).addEventListener('click', (event) => {
     event.preventDefault();
+    document.querySelector('body').style.overflow = 'hidden';
     showModal(modalID);
   });
 }
